aa6544175e592774dc1ba1a594290c943f1d0c1c
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-14 13:46:58 +0700

    Update loops.py

--- fundamental/python_basic_for_security/loops.py
+++ fundamental/python_basic_for_security/loops.py
@@ -1,1 +1,9 @@
+# for loop : check all ports
+for p in [21, 22, 80, 443]:
+  print(f"Checking port {p}..")
 
+# while loop: countdown
+count = 3
+while count > 0:
+  print("countdonw:", count)
+  count -= 1
